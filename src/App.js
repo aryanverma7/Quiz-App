08635f858597590907404ba16e70985035fc74cf
Aryan <aryanadiverma@gmail.com>
2022-09-22 14:52:26 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
+import { useState } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [name, setName]=useState("");
+  const [ques, setQues]=useState();
+  const [score, setScore]=useState(0);
+
+  const fetchQues=async(category='', diff='')=>{
+    const {data}=await axios.get(`https://opentdb.com/api.php?amount=10&${
+    category && `category=${category}`
+    }&${diff && `&difficulty=${diff}`}&type=multiple`
+    );
+    setQues(data.results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App" style={{backgroundImage:"url(./bg.jpg)"}}>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home 
+          name={name} 
+          setName={setName}
+          fetchQues={fetchQues}
+          />}></Route>
+          <Route path='/quiz' element={<Quiz 
+          name={name}
+          ques={ques}
+          score={score}
+          setScore={setScore}
+          setQues={setQues}
+          />}></Route>
+          <Route path='/result' element={<Result
+          score={score}
+          name={name}
+          />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
